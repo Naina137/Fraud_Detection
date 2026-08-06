f1b3c0e4204b8e4ff79c0133915fdebb447e1d54
Naina Gupta <nainaguptaias407@gmail.com>
2026-08-06 22:39:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
 import streamlit as st
-st.set_page_config(page_title="Credit Card Fraud Detection", layout="wide")
-st.title("Credit Card Fraud Detection using Machine Learning")
-st.success("Deployment Successful!")
-st.write("This application demonstrates a Machine Learning model for detecting fraudulent credit card transactions.")
-import streamlit as st
 
 st.set_page_config(page_title="Credit Card Fraud Detection", layout="wide")
 
